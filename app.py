ff35792a11dc4e26064e13ecac9e11961b12ec42
pmunasinghe <pmunasinghe@gmail.com>
2018-08-28 12:37:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 import urllib
 import json
 import os
-import sys
 
 from flask import Flask
 from flask import request
@@ -19,94 +18,69 @@ def webhook():
 
     print("Request:")
     print(json.dumps(req, indent=4))
-    
+
     res = processRequest(req)
-    print("Request processed")
-    
+
     res = json.dumps(res, indent=4)
-    print(res)
-    
+    # print(res)
     r = make_response(res)
-    print(r)
-    
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "createCheckBook":
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://ldcif6u.wdf.sap.corp:44304/sap/opu/odata/sap/ZIVRC_SRV/WorkItems("
-    print(baseurl)
-    
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
-    print("yql_query1:")
-    print(yql_query)
-    
     if yql_query is None:
         return {}
-    yql_url = baseurl + urllib.urlencode({yql_query}) + "?$format=json"
-    print(yql_query)
-    
-    result = urllib.urlopen(baseurl).read()
-    print(result)
+    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+    result = urllib.urlopen(yql_url).read()
     data = json.loads(result)
-    print("data1:")
-    print(data)
-    
     res = makeWebhookResult(data)
     return res
 
 
 def makeYqlQuery(req):
     result = req.get("result")
-    print("result1:")
-    print(result)
     parameters = result.get("parameters")
-    print("parameters1:")
-    print(parameters)
-    city = parameters.get("workitemtype")
-    print("City1:")
-    print(city)
+    city = parameters.get("geo-city")
     if city is None:
         return None
-        
-        return "guid'" + "0005EEE4-48CC-1ED5-B0C9-FA163EA701AC" + "')"
-   #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
-    print("MakeWebhook method")
-    query = data.get('d')
-    print("Query1:")
-    print(query)
+    query = data.get('query')
     if query is None:
         return {}
 
-    result = query.get('WORKITEM_ID')
+    result = query.get('results')
     if result is None:
         return {}
 
-    channel = query.get('DESCRIPTION')
+    channel = result.get('channel')
     if channel is None:
         return {}
 
-   # item = channel.get('item')
-   # location = channel.get('location')
-   # units = channel.get('units')
-   # if (location is None) or (item is None) or (units is None):
-   #    return {}
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
 
-   # condition = item.get('condition')
-   # if condition is None:
-   #     return {}
+    condition = item.get('condition')
+    if condition is None:
+        return {}
 
     # print(json.dumps(item, indent=4))
 
-   # speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-   #         ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-	speech = " The Work Item No. " + result + " has been created for " + channel
-    print("Response1:")
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
     print(speech)
 
     return {
